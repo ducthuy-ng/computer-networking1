1fd05bbf35907676677b5334d5dcf92c932524d4
ducth <ducthuynguyen360@yahoo.com>
2022-03-21 20:39:39 +0700

    Implement RtpPacket.py and its unittests

--- RtpPacket.py
+++ RtpPacket.py
@@ -1,4 +1,5 @@
-from time import time
+import time
+from typing import Union
 
 HEADER_SIZE = 12
 
@@ -7,52 +8,63 @@ class RtpPacket:
     header = bytearray(HEADER_SIZE)
 
     def __init__(self):
-        pass
+        self.header: bytearray = bytearray()
+        self.payload: bytearray = bytearray()
 
-    def encode(self, version, padding, extension, cc, seqnum, marker, pt, ssrc, payload):
+    @staticmethod
+    def encode(version: int, padding: int, extension: int, cc: int, marker: int,
+               payload_type: int, seq_num: int, ssrc: int, payload: bytearray) -> bytearray:
         """Encode the RTP packet with header fields and payload."""
-        timestamp = int(time())
         header = bytearray(HEADER_SIZE)
 
-    # --------------
-    # TO COMPLETE
-    # --------------
-    # Fill the header bytearray with RTP header fields
+        header[0] |= version << 6
+        header[0] |= padding << 5
+        header[0] |= extension << 4
+        header[0] |= cc
 
-    # header[0] = ...
-    # ...
+        header[1] |= marker << 7
+        header[1] |= payload_type
 
-    # Get the payload from the argument
-    # self.payload = ...
+        if seq_num >= (1 << 16):
+            raise OverflowError("Sequence number must in [0 - 65535]")
 
-    def decode(self, byteStream):
+        header[2] = seq_num >> 8
+        header[3] = (0xff & seq_num)
+
+        timestamp = int(time.time()).to_bytes(length=4, byteorder='big')
+        header[4:8] = timestamp
+
+        header[8:12] = ssrc.to_bytes(length=4, byteorder='big')
+
+        return header + payload
+
+    def decode(self, byte_stream: Union[bytes, bytearray]):
         """Decode the RTP packet."""
-        self.header = bytearray(byteStream[:HEADER_SIZE])
-        self.payload = byteStream[HEADER_SIZE:]
+        self.header = bytearray(byte_stream[:HEADER_SIZE])
+        self.payload = byte_stream[HEADER_SIZE:]
 
-    def version(self):
+    def get_version(self):
         """Return RTP version."""
         return int(self.header[0] >> 6)
 
-    def seqNum(self):
+    def get_seq_num(self):
         """Return sequence (frame) number."""
-        seqNum = self.header[2] << 8 | self.header[3]
-        return int(seqNum)
+        return int(self.header[2] << 8 | self.header[3])
 
-    def timestamp(self):
+    def get_timestamp(self):
         """Return timestamp."""
         timestamp = self.header[4] << 24 | self.header[5] << 16 | self.header[6] << 8 | self.header[7]
         return int(timestamp)
 
-    def payloadType(self):
+    def get_payload_type(self):
         """Return payload type."""
         pt = self.header[1] & 127
         return int(pt)
 
-    def getPayload(self):
+    def get_payload(self):
         """Return payload."""
         return self.payload
 
-    def getPacket(self):
+    def get_packet(self):
         """Return RTP packet."""
         return self.header + self.payload
